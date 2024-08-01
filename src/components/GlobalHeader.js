733e8e72d9eb8d1b3e6307ff2371578ac2dd6a55
mattlohkamp <work@mattlohkamp.com>
2024-07-31 22:16:03 -0700

    options for links

--- src/components/GlobalHeader.js
+++ src/components/GlobalHeader.js
@@ -4,11 +4,28 @@ export default function GlobalHeader() {
 		<header>
 			<h1>Matt Lohkamp</h1>
 			<h2>Interactive Portfolio</h2>
-			<a href="/dox/Matt Lohkamp's Resume.pdf">Resume (pdf)</a>
-			<a href="/dox/Matt Lohkamp's Resume.txt">Resume (txt)</a>
-			<a href="https://www.linkedin.com/in/mattlohkamp/">LinkedIn</a>
-			<a href="https://github.com/mattlohkamp">GitHub</a>
-			<a href="https://stackoverflow.com/users/14026/matt-lohkamp">
+			<a href="/dox/Matt Lohkamp's Resume.pdf" download>
+				Resume (pdf)
+			</a>
+			<a href="/dox/Matt Lohkamp's Resume.txt" download>
+				Resume (txt)
+			</a>
+			<a
+				href="https://www.linkedin.com/in/mattlohkamp/"
+				target="_blank"
+				rel="noopener noreferrer">
+				LinkedIn
+			</a>
+			<a
+				href="https://github.com/mattlohkamp"
+				target="_blank"
+				rel="noopener noreferrer">
+				GitHub
+			</a>
+			<a
+				href="https://stackoverflow.com/users/14026/matt-lohkamp"
+				target="_blank"
+				rel="noopener noreferrer">
 				StackOverflow
 			</a>
 		</header>
