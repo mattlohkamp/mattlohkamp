7d018b80ba0aa0ad432718d7de006578105f7146
mattlohkamp <work@mattlohkamp.com>
2024-07-31 22:16:19 -0700

    messin around with nametag styling

--- src/components/HelloFeature.js
+++ src/components/HelloFeature.js
@@ -1,4 +1,16 @@
 import React from "react";
+import "./HelloFeature.module.css";
 export default function HelloFeature() {
-	return <></>;
+	return (
+		<div className="nametag">
+			<div className="nametag-header">Hello, my name is</div>
+			<div className="nametag-body">
+				<div className="name">
+					{"<Matt Lohkamp />".split("").map((val, i) => (
+						<span key={i}>{val}</span>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
