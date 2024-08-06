b663240c8a5b28981ebce0e0ebf9269151f3a744
mattlohkamp <work@mattlohkamp.com>
2024-08-06 12:56:26 -0700

    make this module css mess work

--- src/components/HelloFeature.js
+++ src/components/HelloFeature.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./HelloFeature.module.css";
+import * as styles from "./HelloFeature.module.css";
 export default function HelloFeature() {
 	return (
-		<div className="nametag">
-			<div className="nametag-header">Hello, my name is</div>
-			<div className="nametag-body">
-				<div className="name">
+		<div className={styles["nametag"]}>
+			<div className={styles["nametag-header"]}>Hello, my name is</div>
+			<div className={styles["nametag-body"]}>
+				<div className={styles["nametag-name"]}>
 					{"<Matt Lohkamp />".split("").map((val, i) => (
 						<span key={i}>{val}</span>
 					))}
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 export default {
+	//	cache: false,
 	entry: path.join(__dirname, "src", "index.js"),
 	output: {
 		path: path.resolve(__dirname, "dist"),
@@ -31,7 +32,22 @@ export default {
 				},
 			},
 			{
+				test: /\.module\.css$/,
+				use: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: {
+							modules: {
+								localIdentName: "[name]__[local]___[hash:base64:5]",
+							},
+						},
+					},
+				],
+			},
+			{
 				test: /\.css$/,
+				exclude: /\.module\.css$/,
 				use: ["style-loader", "css-loader"],
 			},
 			{
@@ -43,6 +59,9 @@ export default {
 				use: ["@svgr/webpack"],
 			},
 		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
