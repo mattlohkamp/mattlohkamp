a6ba5f4afd8807d9748386583a31f24d9de91dfe
mattlohkamp <work@mattlohkamp.com>
2024-08-07 23:17:25 -0700

    externalize strings

--- src/components/HelloFeature.js
+++ src/components/HelloFeature.js
@@ -1,11 +1,14 @@
 import * as styles from "./HelloFeature.module.css";
+import strings from "../../public/data/site.json";
 export default function HelloFeature() {
 	return (
 		<div className={styles["nametag"]}>
-			<div className={styles["nametag-header"]}>Hello, my name is</div>
+			<div className={styles["nametag-header"]}>
+				{strings.HelloFeature["nametag-label"]}
+			</div>
 			<div className={styles["nametag-body"]}>
 				<div className={styles["nametag-name"]}>
-					{"<Matt Lohkamp />".split("").map((val, i) => (
+					{strings.HelloFeature["nametag-name"].split("").map((val, i) => (
 						<span key={i}>{val}</span>
 					))}
 				</div>
